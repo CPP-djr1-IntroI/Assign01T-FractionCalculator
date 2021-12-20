bd93d97a6ce9f5bf660c46c56af4baa43a4c274f
djr1 <djr1@DESKTOP-KMQOKIB>
2021-12-19 21:51:19 -0800

    Add project files.

---
+++ FractionCalculator/main.cpp
@@ -0,0 +1,18 @@
+//**********************************************************************
+// File:				main.cpp
+// Author:			
+// Date:				
+// Class:				Intro to C++
+// Assignment:	Assign 01 - Fraction Calculator
+// Purpose:			
+// Hours:				
+//**********************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+	return EXIT_SUCCESS;
+}
